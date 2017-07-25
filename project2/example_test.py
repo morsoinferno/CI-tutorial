1c7e536305c922ca843e7c323052a568e154249a
cristobal <cristobal.acosta@usach.cl>
2017-07-25 14:19:21 -0400

    se añade tag v0.1

--- project2/example_test.py
+++ project2/example_test.py
@@ -1,26 +1,17 @@
-import unittest
-from example import app, db
+import unittest  
+from example import app
 
-TEST_SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+class appTestCase(unittest.TestCase):  
+    def setUp(self):
+        self.app = app.test_client()
+        app.config["TESTING"] = True
 
-class appTestCase(unittest.TestCase):
-	def setUp(self):
-		self.app = app.test_client()
-		app.config["TESTING"] = True
+    def tearDown(self):
+        pass
 
-		app.config["SQLALCHEMY_DATABASE_URI"] = TEST_SQLALCHEMY_DATABASE_URI
-		db.create_all()
-		db.session.commit()
+    def test_get_tasks(self):
+        response = self.app.get("/api/v0.1/tasks")
+        self.assertEqual(200, response.status_code)
 
-	def tearDown(self):
-		db.session.remove()
-		db.drop_all()
-
-	def test_get_tasks(self):
-
-		response = self.app.get("/api/v0.1/tasks")
-		
-		self.assertEqual(200, response.status_code)
-		
-if __name__ == '__main__':
+if __name__ == '__main__':  
     unittest.main()
